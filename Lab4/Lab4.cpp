400f1d05f2da81ddaa79e69de33b54a01303a633
KELYN THOMAS <5085259@nwpolytech.ca>
2024-10-10 16:31:45 -0600

    Finished Program #1

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -3,21 +3,29 @@
     
 using namespace std;
 
+int credits = 100;
+int dieOne;
+int dieTwo;
+
 int die() {
-    int dieOne = rand()%5+1;
-    int dieTwo = rand() % 5 + 1;
+    dieOne = rand()%5+1;
+    dieTwo = rand() % 5 + 1;
     return dieOne + dieTwo;
 }
 
-void playGame(int dieThrow) {
+void playGame(int dieThrow, int wager) {
     int point;
-    cout << "Player threw: "<< dieThrow << " \n\n";
+    cout << "\nplayer rolled " << dieOne << " + " << dieTwo << " = " << dieOne + dieTwo << "\n";
 
     if (dieThrow == 7 || dieThrow == 11) {
         cout << "Player wins!: " << dieThrow << " \n";
+        credits += wager;
+        cout << "\nyou won " << wager << " credits. Your new balance is " << credits << " credits";
     }
     else if (dieThrow == 2 || dieThrow == 3 || dieThrow == 12) {
-        cout << "Craps!: " << dieThrow << " \n";
+        cout << "Player loses!: " << dieThrow << " \n";
+        credits -= wager;
+        cout << "\nyou lost " << wager << " credits. Your new balance is " << credits << " credits";
     }
     else{
         point = dieThrow;
@@ -26,18 +34,20 @@ void playGame(int dieThrow) {
 ;        while (secondThrow != point) {
 
             if (secondThrow != 7 && secondThrow != point) {
-                cout << "Rolling again!\n";
                 secondThrow = die();
-                cout << "Player threw: " << secondThrow << " \n";
             }
             
             if (secondThrow == 7) {
                 cout << "Player loses!: " << secondThrow << " \n";
+                credits -= wager;
+                cout << "\nyou lost " << wager << " credits. Your new balance is " << credits << " credits";
                 break;
             }
 
             if (secondThrow == point) {
                 cout << "Player Wins!: " << secondThrow << " \n";
+                credits += wager;
+                cout << "\nyou won " << wager << " credits. Your new balance is " << credits << " credits";
                 break;
             }
 
@@ -49,14 +59,34 @@ void playGame(int dieThrow) {
 int main() {
     char ans;
     bool done = false;
+    int wager = 0;
+    bool enoughCredits = false;
+
     while (!done) {  // YOU MUST WRITE THIS FUNCTION
-        cout << " Play again (y/n) ? \n";
-        cin >> ans;
-        if (ans == 'n' || ans == 'N') done = true;
-        else {
-         done = false;
-         playGame(die());
+
+        while (!enoughCredits) {
+            cout << "You have " << credits << " credits.How much do you wager ? ";
+            cin >> wager;
+
+            if (wager > credits) {
+                cout << "your wager exceeds your balance.Try again.\n";
+                enoughCredits = false;
+            }
+            else enoughCredits = true;
         }
+
+        enoughCredits = false;
+
+        playGame(die(), wager);
+
+        cout << "\nPlay again (y/n) ? \n";
+        cin >> ans;
+
+        if (ans == 'n' || ans == 'N') {
+            done = true; 
+        }
+        else done = false;
+        
     }
     
     return 0;
